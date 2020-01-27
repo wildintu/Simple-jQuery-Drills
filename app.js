1eab9aabf401e5b5cbde9fbcac6173d9469864cd
wildintu <hello@wildintu.com>
2020-01-27 11:03:36 -0600

    h2 stuff

--- app.js
+++ app.js
@@ -13,10 +13,24 @@ $(document).ready(function(){
         }
     });
 
-    $(<div></div>).appendTo('body');
-    $()
+    $('<div></div>').appendTo('body');
+    $('<h2></h2>').appendTo('div');
 
-        
+    $("form").submit(function(e) {
+        e.preventDefault();
+        let msg = $('#message').val();
+        $('h2').append(`<ul>${msg}</h2>`);
+    });
     
+    $("h2").mouseover(function (event) {
+        console.log(event);
+        $(event.target).css({'background-color': getRandomColor(), 'border-radius': '30px'});
+    })
 
-})
+});
+
+function getRandomColor() {
+    let color = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+    return color;
+}
+
